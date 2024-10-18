cbef7eca8c3ede41b10f17ab46e5a3e456ac5e42
EzechielKiregha <ekiregha@gmail.com>
2024-10-18 02:46:23 +0200

    optional column this means it can be none.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """sql model User for database users
 
    -attributes:
@@ -10,29 +11,24 @@
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from typing import Optional
 
 Base = declarative_base()
 
-
 class User(Base):
-    """
-    Represents a user in the system.
+        """
+        SQLAlchemy model representing the 'users' table.
 
-    Attributes:
-        id (int): The primary key for the user.
-        email (str): The user's email address (max 250 chars).
-        hashed_password (str): The hashed password for the user (max 250
-        chars).
-        session_id (Optional[str]): A session ID for tracking user sessions
-        (nullable, max 250 chars).
-        reset_token (Optional[str]): A token for password reset functionality
-        (nullable, max 250 chars).
-    """
-    __tablename__: str = 'users'
+        Attributes:
+            id (int): The primary key for the user.
+            email (str): The user's email address (max 250 chars, non-nullable).
+            hashed_password (str): The user's hashed password (max 250 chars, non-nullable).
+            session_id (str): A session ID for tracking user sessions (nullable, max 250 chars).
+            reset_token (str): A token for password reset functionality (nullable, max 250 chars).
+        """
+        __tablename__ = 'users'
 
-    id: Column = Column(Integer, primary_key=True)
-    email: Column = Column(String(255), nullable=False)
-    hashed_password: Column = Column(String(255), nullable=False)
-    session_id: Column = Column(String(255), nullable=True)
-    reset_token: Column = Column(String(225), nullable=True)
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        email = Column(String(250), nullable=False)
+        hashed_password = Column(String(250), nullable=False)
+        session_id = Column(String(250), nullable=True)
+        reset_token = Column(String(250), nullable=True)
